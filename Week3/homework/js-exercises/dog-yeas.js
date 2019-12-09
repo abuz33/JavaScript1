5c4f88bd69a0a05bd3fc85f60d746c834a6e445f
Abuzer ALACA <aalaca.33@gmail.com>
2019-12-09 17:43:55 +0100

    Added dog-age.js file

---
+++ Week3/homework/js-exercises/dog-yeas.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function calculateDogAge(ageDog) {
+  return `Your doggie is ${ageDog * 7} years old in dog years!`;
+}
+
+console.log(calculateDogAge(7));
+console.log(calculateDogAge(8));
+console.log(calculateDogAge(9));
